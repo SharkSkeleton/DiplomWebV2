695a5dfa84a90438c5e6b1cf80536a78f7ff5d50
SharkSkeleton <mishamyagkiy3@gmail.com>
2020-05-05 17:48:49 +0300

    Создана форма для входа
    entry-form.component.css:
    Добавлены стили для полей и кнопок
    
    entry-form.component.html:
    Добавлены 2 поля и 2 кнопки
    Поле Email: имеет верификацию на введённую строку
    Поле Password: имеет кнопку (переключатель) для отображения/сокрытия введённого текста
    
    entry-form.component.ts:
    Переменная hide = true; для того, чтобы текст который вводиться в поле Password был скрыт по умолчанию
    Переменная email - отвечает за проверку валидности текста в поле Email
    Метод GetErrorMessage() - возвращает результат валидацию текста.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { EntryFormComponent } from './entry-form/entry-form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
+    EntryFormComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    DragDropModule
+    DragDropModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/entry-form/entry-form.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.css']
+})
+export class EntryFormComponent {
+  hide = true;
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+}
