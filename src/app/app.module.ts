2bfa1c49d47058915918bf9d955420f72b1b9fe5
SharkSkeleton <mishamyagkiy3@gmail.com>
2020-05-05 16:44:14 +0300

    Начало разработки по установленным в Trello задачам

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodoComponent } from './todo/todo.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
   ],
   imports: [
     BrowserModule,
